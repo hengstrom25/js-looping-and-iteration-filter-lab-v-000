5feb219afa36fba79cb0b66ca5fab5b399a12892
Heidi Engstrom <hengstrom25@gmail.com>
2018-11-08 19:30:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+  return drivers.filter(function(driver) {return driver.toLowerCase() === string.toLowerCase();})
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver) {return driver.startsWith(string);})
+}
